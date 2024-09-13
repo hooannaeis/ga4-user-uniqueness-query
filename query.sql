5b9e16bdb659c9ac992976ba34065247282d013e
Hannes Kuhl <35167216+hooannaeis@users.noreply.github.com>
2024-09-13 08:55:38 +0200

    Update query.sql

--- query.sql
+++ query.sql
@@ -1,7 +1,7 @@
 DECLARE
-  event_param_query DEFAULT "SELECT CONCAT('ep.', ep.key) event_params_key, CAST(ep.value.int_value AS string), CAST(ep.value.float_value AS string), CAST( ep.value.double_value AS string)) event_params_value,  COUNT(DISTINCT user_pseudo_id) distinct_user_count, COUNT(*) AS distinct_value_count, RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_value_count FROM `clothing-tracking.analytics_447784243.events_*` LEFT JOIN UNNEST(event_params) ep WHERE _table_suffix >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)) GROUP BY all UNION ALL ";
+  event_param_query DEFAULT "SELECT CONCAT('ep.', ep.key) event_params_key, CAST(ep.value.int_value AS string), CAST(ep.value.float_value AS string), CAST( ep.value.double_value AS string)) event_params_value,  COUNT(DISTINCT user_pseudo_id) distinct_user_count, COUNT(*) AS distinct_value_count, RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_value_count FROM `{{project-id}}.{{dataset_id}}.events_*` LEFT JOIN UNNEST(event_params) ep WHERE _table_suffix >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)) GROUP BY all UNION ALL ";
 DECLARE
-  user_param_query DEFAULT "SELECT CONCAT('up.', up.key) event_params_key, COALESCE(up.value.string_value, CAST(up.value.int_value AS string), CAST(up.value.float_value AS string), CAST( up.value.double_value AS string)) event_params_value,  COUNT(DISTINCT user_pseudo_id) distinct_user_count, COUNT(*) AS distinct_value_count, RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_value_count FROM `clothing-tracking.analytics_447784243.events_*` LEFT JOIN UNNEST(user_properties) up WHERE _table_suffix >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)) GROUP BY all UNION ALL ";
+  user_param_query DEFAULT "SELECT CONCAT('up.', up.key) event_params_key, COALESCE(up.value.string_value, CAST(up.value.int_value AS string), CAST(up.value.float_value AS string), CAST( up.value.double_value AS string)) event_params_value,  COUNT(DISTINCT user_pseudo_id) distinct_user_count, COUNT(*) AS distinct_value_count, RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_value_count FROM `{{project-id}}.{{dataset_id}}.events_*` LEFT JOIN UNNEST(user_properties) up WHERE _table_suffix >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)) GROUP BY all UNION ALL ";
 DECLARE
   full_query STRING DEFAULT CONCAT(event_param_query, user_param_query);
 DECLARE
@@ -14,7 +14,7 @@ SET
   SELECT
     ARRAY_AGG(DISTINCT field_path) -- field_path is for example device.category, with column_name being device
   FROM
-    `clothing-tracking.analytics_447784243.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
+    `{{project-id}}.{{dataset_id}}.INFORMATION_SCHEMA.COLUMN_FIELD_PATHS`
   WHERE
     table_name LIKE 'events_20%'
     AND NOT REGEXP_CONTAINS(data_type, r"(ARRAY|STRUCT)") -- we dont want ARRAYs because they have to be unnested before accesssing. We dont want STRUCTs because we just want the specfific paths in the STRUCT
@@ -31,7 +31,7 @@ SET
   OFFSET
     (i)], "' AS event_params_key, CAST(", cols[
   OFFSET
-    (i)]," as string) as event_params_value,  COUNT(DISTINCT user_pseudo_id) distinct_user_count, COUNT(*) AS distinct_value_count, RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_value_count FROM `clothing-tracking.analytics_447784243.events_*` WHERE _table_suffix >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)) group by all" );
+    (i)]," as string) as event_params_value,  COUNT(DISTINCT user_pseudo_id) distinct_user_count, COUNT(*) AS distinct_value_count, RANK() OVER (ORDER BY COUNT(*) DESC) AS rank_value_count FROM `{{project-id}}.{{dataset_id}}.events_*` WHERE _table_suffix >= FORMAT_DATE('%Y%m%d', DATE_SUB(CURRENT_DATE(), INTERVAL 7 DAY)) group by all" );
 IF
   i < ARRAY_LENGTH (cols) - 1 THEN
 SET
